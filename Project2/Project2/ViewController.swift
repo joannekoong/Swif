8eb05e874b75009502fca65469ffae41333c3f5c
Joanne <Joanne@Joannes-MacBook-Pro-2.local>
2015-11-08 20:29:12 -0800

    project 2 finished

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2015 joanne. All rights reserved.
 //
 
+import GameplayKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -17,6 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button3: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
     
     
@@ -31,16 +33,37 @@ class ViewController: UIViewController {
         button1.layer.borderColor = UIColor.lightGrayColor().CGColor
         button2.layer.borderColor = UIColor.lightGrayColor().CGColor
         button3.layer.borderColor = UIColor.lightGrayColor().CGColor
-        askQuestion()
+        askQuestion(nil)
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
         
         button1.setImage(UIImage(named: countries[0]), forState: .Normal)
-        button2.setImage(UIImage(named: countries[2]), forState: .Normal)
-        button3.setImage(UIImage(named: countries[3]), forState: .Normal)
-    
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        title = countries[correctAnswer].uppercaseString
     }
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            ++score
+        }
+        else {
+            title = "Incorrect"
+            --score
+        }
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
+        presentViewController(ac, animated: true, completion: nil)
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
