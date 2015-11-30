75fb95a05305bc12e603e5c9e65c1c3d636da335
Joanne <Joanne@Joannes-MacBook-Pro-2.local>
2015-11-29 23:32:14 -0500

    prototype for table view cell UI finished

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Joanne on 11/29/15.
+//  Copyright © 2015 joanne. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
